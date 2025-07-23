221b15bc752076b849a9c9eea5622e11bf67b7eb
KyuSang Jang <charlie.jang515@gmail.com>
2025-07-24 06:26:29 +0900

    added RateLimitClient to __init__.py

--- riot_api/__init__.py
+++ riot_api/__init__.py
@@ -6,5 +6,7 @@ __author__ = "Charlie.Jang"
 __version__ = "0.0.1"
 
 from riot_api.client import Client
+from riot_api.rate_limit_client import RateLimitClient
 
-__all__ = ["Client"]
+
+__all__ = ["Client", "RateLimitClient"]
